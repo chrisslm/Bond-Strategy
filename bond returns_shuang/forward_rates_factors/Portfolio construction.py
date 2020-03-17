8ea2005c75cf9f9f8be25ee10ea59b1bc551ce05
Shuang Zhao <shuzhao@student.ethz.ch>
2020-03-17 17:01:48 +0100

    forward backtest update

--- bond returns_shuang/forward_rates_factors/Portfolio construction.py
+++ bond returns_shuang/forward_rates_factors/Portfolio construction.py
@@ -11,7 +11,7 @@ from datetime import datetime
 from sklearn.linear_model import LinearRegression
 import statsmodels.api as sm
 from sklearn.metrics import r2_score
-
+#%%
 # import and processing data
 ############################################################
 return1=pd.read_csv('Australian_return.csv')
@@ -194,29 +194,97 @@ return1=return1.loc[factor1.index]
 factor1['return']=return1['10Y']
 US=factor1.dropna(axis=0,how='any')
 ############################################################
+#%%
+US=US.loc[US.index.year>=1995]
+US=US.iloc[:-1,:]
+US.index=German.index
+Australian=Australian.loc[Australian.index.year>=1995]
 country=[Australian,Canada,China,France,German,Italy,Japan, South_korea,Spain, Swiss,UK,US]
-
+country_name=['Australian','Canada','China','France','German','Italy','Japan', 'South_korea','Spain', 'Swiss','UK','US']
 # basic regression
+#%% riskfree
+rf=pd.read_csv('fama_french.csv')
+rf['Dates']=rf['Dates'].astype(str)
+rf['Dates']=rf['Dates'].apply(lambda x:datetime.strptime(x, '%Y%m'))
+rf=rf.iloc[823:,:]
+rf=rf.set_index('Dates')
+rf.index=German.index
+rf=(rf/100).astype(np.float64)
 
 
-
+for df in country:
+    df['excess_return']=df['return']-rf.loc[df.index]['RF']
+#%%
 for df in country:
     #for out of sample, expanding window
     n=60
     pre1=np.zeros(len(df))
     pre2=np.zeros(len(df))
     for i in range(n,len(df)):
-        Y=df['return'].values[:i]
-        X1=df.iloc[:i,:-3].values
+        Y=(df['excess_return']).values[:i]
+        X1=df.iloc[:i,:-4].values
         X2=df['spread'].values[:i].reshape(-1,1)
         lr1=LinearRegression().fit(X1,Y)
         lr2=LinearRegression().fit(X2,Y)
         #CP
-        pre1[i]=lr1.predict(df.iloc[i,:-3].values.reshape(1,-1))[0]
+        pre1[i]=lr1.predict(df.values[i,:-4].reshape(1,-1))[0]
         #fama-bliss
         pre2[i]=lr2.predict(df['spread'].values[i].reshape(1,-1))[0]
     df['pre_CP']=pre1
     df['pre_FB']=pre2
+#%%
+CP=pd.DataFrame()
+CP['Dates']=German.index
+FB=pd.DataFrame()
+FB['Dates']=German.index
+returns=pd.DataFrame()
+returns['Dates']=German.index
+for i in range(len(country)):
+    df=country[i]
+    cp1=pd.DataFrame()
+    cp1['Dates']=df.index
+    cp1[country_name[i]]=df['pre_CP'].values
+    fb1=pd.DataFrame()
+    fb1['Dates']=df.index
+    fb1[country_name[i]]=df['pre_FB'].values
+    return01=pd.DataFrame()
+    return01['Dates']=df.index
+    return01[country_name[i]]=df['return'].values
+    CP=pd.merge(CP,cp1,on='Dates',how='outer')
+    FB=pd.merge(FB,fb1,on='Dates',how='outer')
+    returns=pd.merge(returns,return01,on='Dates',how='outer')
+#%%    
+returns=returns.set_index('Dates')
+returns=returns.fillna(0)
+FB=FB.set_index('Dates')
+FB=FB.sort_index()
+FB=FB.loc[FB.index.year>=2003]
+FB=FB.fillna(-1)
+returns_FB=returns.loc[FB.index]
+holding1=(FB>np.dot(np.ones([np.shape(FB)[1],1]),FB.quantile(q=2/3,axis=1).values.reshape(1,-1)).T)
+Portfolio_FB=returns_FB*holding1
+Portfolio_FB=Portfolio_FB.sum(axis=1)/holding1.sum(axis=1)
+#%%
+CP=CP.set_index('Dates')
+CP=CP.sort_index()
+CP=CP.loc[CP.index.year>=2003]
+CP=CP.fillna(-1)
+b=CP
+returns_CP=returns.loc[CP.index]
+holding2=(CP>np.dot(np.ones([np.shape(FB)[1],1]),CP.quantile(q=2/3,axis=1).values.reshape(1,-1)).T)
+Portfolio_CP=returns_CP*holding2
+p2=returns_FB*holding2
+Portfolio_CP=Portfolio_CP.sum(axis=1)/holding2.sum(axis=1)
+p2=(1+Portfolio_CP).cumprod()
+EW=returns_CP.sum(axis=1)/(returns_CP!=0).sum(axis=1)
 
-
-
+r2=np.zeros([np.shape(CP)[1],1])
+for i in range(np.shape(CP)[1]):
+    r2[i]=r2_score(CP.iloc[:,i].values.reshape(-1,1),returns_CP.iloc[:,i].values.reshape(-1,1))
+#%%
+plt.figure()
+plt.plot(EW.index,(1+EW.values).cumprod())
+plt.plot(EW.index,(1+Portfolio_CP.values).cumprod())
+plt.plot(EW.index,(1+Portfolio_FB.values).cumprod())
+plt.legend(['EW','CP','FB'])
+plt.show()
